aeef475d3a706c29cbebb0b1f0e24784d0d9829c
Henry Liu <11.henrybliu@gmail.com>
2023-07-07 15:27:24 -0700

    completed navBar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { NavLink as Link } from "react-router-dom";
 import "./navbar.css";
-import jaqueminot from "../../images/jaqueminot.png";
+import jaqueminotBorder from "../../images/jaqueminotBorder.png";
 
 const Navbar = () => {
   return (
-    <div class="navbarContainer">
-      <div class="leftNavbar">
-        <Link to="/">
-          <img src={jaqueminot} alt="jaqueminot" />
-        </Link>
-      </div>
-      <div class="rightNavbar">
-        <Link to="/about">About Us</Link>
-        <Link to="/rush">Rush</Link>
-        <Link to="/brothers">Brothers</Link>
+    <div class="navbar">
+      <div class="navbarContainer">
+        <div class="leftNavbar">
+          <Link to="/">
+            <img src={jaqueminotBorder} alt="jaqueminot" />
+          </Link>
+        </div>
+        <div class="rightNavbar">
+          <Link to="/about">About Us</Link>
+          <Link to="/rush">Rush</Link>
+          <Link to="/brothers">Brothers</Link>
+        </div>
       </div>
     </div>
   );
