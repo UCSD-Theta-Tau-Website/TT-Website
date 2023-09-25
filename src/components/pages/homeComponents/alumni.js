f1d1aeb3c34e4d9a13ee0170610a7774c19c0050
andrewt319 <andrew319t@gmail.com>
2023-09-24 17:21:51 -0700

    added skeleton of alumni carousel

--- src/components/pages/homeComponents/alumni.js
+++ src/components/pages/homeComponents/alumni.js
@@ -1,10 +1,15 @@
 import React from "react";
 import '../../../style/alumni.css'
+import AlumniCarousel from './alumniCarousel';
 
 const Alumni = () => {
     return (
         <div className="alumni">
-            
+            <div className="title">
+                <h1>Our Alumni</h1>
+                <div className="titleLine"></div>
+            </div>
+            <AlumniCarousel/>
         </div>
     )
 }
