c07352a2474c2ea305dcbc4da11284a708254d80
andrewt319 <andrew319t@gmail.com>
2023-10-07 20:47:44 -0700

    home page update

--- src/components/pages/homeComponents/alumniCarousel.js
+++ src/components/pages/homeComponents/alumniCarousel.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import '../../../style/alumniCarousel.css'
-import alumniResponses from "../../../activeData/info/alumniResponses.json";
+import originalAlumniResponses from "../../../activeData/info/alumniResponses.json";
 import { IoIosArrowBack , IoIosArrowForward } from 'react-icons/io';
 import AlumniCard from './alumniCard';
 
 
 const AlumniCarousel = () => {
     const [current, setCurrent] = useState(0);
+
+    const [alumniResponses, setAlumniResponses] = useState([]);
+
+    useEffect(() => {
+        const shuffledResponses = [...originalAlumniResponses];
+        
+        for (let i = shuffledResponses.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledResponses[i], shuffledResponses[j]] = [shuffledResponses[j], shuffledResponses[i]];
+        }
+        
+        setAlumniResponses(shuffledResponses);
+    }, []);
+
     const length = alumniResponses.length;
 
     const nextSlide = () => {
@@ -23,6 +37,7 @@ const AlumniCarousel = () => {
             <section className="slider">
                 <IoIosArrowBack className="left-arrow" onClick={prevSlide}/>
                 {alumniResponses.map((alumni, index) => {
+                    console.log(alumni);
                     return (
                         <div className={index === current ? 'slide active' : 'slide'} key={index}>
                             {index === current && 
