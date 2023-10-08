6a335284b2e6c6b4d0ea1a11c1f9222784ed86eb
Jordan Angin <jordanangin7@gmail.com>
2023-10-08 01:50:34 -0700

    updates

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -26,18 +26,18 @@ const About = () => {
             <div className="grow border-2 border-red-800"></div>
           </div>
           <div className="grow flex flex-col-reverse items-center md:flex-row pt-8 md:py-8 md:mb-6 justify-center">
-            <div className="bg-flower2 md:bg-flower bg-cover italic w-full md:w-1/2 px-12 md:mr-8">
+            <div className="bg-flower2 md:bg-flower bg-center bg-no-repeat bg-contain italic w-full md:w-1/2 px-12 md:mr-8">
               <p className="text-lg md:text-2xl py-12 md:h-96 md:py-96 leading-8 md:leading-10 text-center flex flex-row items-center font-extralight italic">Our mission is to develop engineers into socially responsible professionals and leaders of the industry. Our fraternity provides members with unique opportunities for networking, professional development, community service, and lifelong friendships. We are committed to promoting diversity and inclusivity within the engineering community and beyond.</p>
             </div>
             <div className="bg-rectangle4 bg-cover bg-left w-full h-96 md:py-96 md:w-1/2 rounded-none md:rounded-l-xl 3xl:rounded-xl"></div>
           </div>
-          <div className="pb-4 md:pb-10">
+          <div className="">
             <div className="grow border-2 border-red-800"></div>
           </div>
         </div>
 
         {/* huge photo */}
-        <div className="bg-biglitto bg-cover bg-bottom w-full height"></div>
+        <div className="bg-biglitto bg-cover bg-bottom rounded-none w-screen height"></div>
 
         {/* our history */}
         <div className="flex flex-col py-10 height">
@@ -46,20 +46,20 @@ const About = () => {
             <div className="grow border-2 border-red-800"></div>
           </div>
           <div className="grow flex flex-col-reverse items-center md:flex-row pt-8 md:py-8 md:mb-6 justify-center">
-            <div className="bg-flower2 md:bg-flower bg-cover italic w-full md:w-1/2 px-12 md:mr-8 h-auto">
-              <p className="text-lg md:text-2xl py-12 md:h-96 md:py-96 leading-8 md:leading-10 text-center py-12 flex flex-row items-center font-extralight italic">In the Spring of 2009, several students of UCSD’s Jacobs School of Engineering felt that there was a lack of engineering representation in the Greek community on campus. So, thirteen founding fathers took the first step towards establishing a chapter of the nation’s oldest and largest professional engineering fraternity on the UCSD campus. On November 20th, 2010, UCSD was installed as the Epsilon Delta Chapter of Theta Tau.</p>
+            <div className="bg-flower2 md:bg-flower bg-center bg-no-repeat bg-contain italic w-full md:w-1/2 px-12 md:mr-8">
+              <p className="text-lg md:text-2xl py-12 md:h-96 md:py-96 leading-8 md:leading-10 text-center flex flex-row items-center font-extralight italic">In the Spring of 2009, several students of UCSD’s Jacobs School of Engineering felt that there was a lack of engineering representation in the Greek community on campus. So, thirteen founding fathers took the first step towards establishing a chapter of the nation’s oldest and largest professional engineering fraternity on the UCSD campus. On November 20th, 2010, UCSD was installed as the Epsilon Delta Chapter of Theta Tau.</p>
             </div>
-            <div className="bg-biggrass bg-cover bg-center w-full h-96 md:py-96 md:w-1/2 rounded-none md:rounded-l-xl 3xl:rounded-xl"></div>
+            <div className="bg-biggrass bg-cover bg-left w-full h-96 md:py-96 md:w-1/2 rounded-none md:rounded-l-xl 3xl:rounded-xl"></div>
           </div>
-          <div className="pb-4 md:pb-10">
+          <div className="">
             <div className="grow border-2 border-red-800"></div>
           </div>
         </div>
 
         {/* our pillars */}
-        <div className="flex flex-col items-center justify-center height md:-mt-20">
+        <div className="flex flex-col items-center justify-center height">
           <h1 className="text-4xl sm:text-5xl px-10 text-red-800 abhaya">Our Pillars</h1>
-          <div className="pt-10 pb-10">
+          <div className="pt-10 pb-4 md:pb-10">
             <div className="border w-36 border-red-800 my-5"></div>
           </div>
           <div className="flex flex-col items-start lg:flex-row pb-12 mx-12">
